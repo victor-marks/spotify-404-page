25a386b618dc70bced9b10035a1f217508810b01
Victor Marks <vjmarks@gmail.com>
2019-08-14 23:29:14 -0700

    aligned navbar properly

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
 import './Navbar.css';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 // import logo from './images/logo.svg';
 
 class Navbar extends Component {
   render() {
     return (
       <div className="navbar">
-        <ul>
-          <li>Spotify</li>
-        </ul>
+        <div className="navbar-left">
+          <ul>
+            <li>
+              <NavLink className="nav-item" to="/">
+                Spotify
+              </NavLink>
+            </li>
+          </ul>
+        </div>
 
-        <ul>
-          <li>Premium</li>
-          <li>Help</li>
-          <li>Download | </li>
-          <li>
-            <FontAwesomeIcon icon={faUserCircle} />
-            Profile
-          </li>
-        </ul>
+        <div className="navbar-right">
+          <ul>
+            <li>
+              <NavLink className="nav-item" to="/">
+                Premium
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-item" to="/">
+                Help
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-item" to="/">
+                Download
+              </NavLink>
+            </li>
+            <li>|</li>
+            <li>
+              <NavLink className="nav-item" to="/">
+                <FontAwesomeIcon className="icon" icon={faUserCircle} />
+                Profile
+                <FontAwesomeIcon className="icon" icon={faChevronDown} />
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
